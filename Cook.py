8304c981eec104e6d1f6315483644ed79b7eca5f
BrucePB <33796338+BrucePB@users.noreply.github.com>
2018-01-08 13:37:51 +0800

    fo

---
+++ Cook.py
@@ -0,0 +1,17 @@
+#/bin/python3
+
+from PIL import Image,ImageDraw,ImageFont
+
+
+def add_number(img):
+    draw = ImageDraw.Draw(img)
+    myfont = ImageFont.truetype("C:\\Users\\dell\\Desktop\\Python-master\\simhei.ttf",80)
+    fillcolor = "#00ff00"
+    width,height = img.size
+    draw.text((width-50,10),'V',font = myfont,fill = fillcolor)
+    img.save('result.jpg','jpeg')
+
+
+if __name__=='__main__':
+    image = Image.open()
+    add_number(image)
